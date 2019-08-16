c7476d7e830064bf16f0f661c2c2bb519f697a48
Hirohisa AMAN <aman@computer.org>
2019-08-16 14:17:28 +0900

    Moved from Metrics_Tools project.

---
+++ src/org/computer/aman/metrics/util/java/CodeLineMapForJava.java
@@ -0,0 +1,162 @@
+package org.computer.aman.metrics.util.java;
+
+import org.computer.aman.metrics.util.CodeLineMap;
+
+/**
+ * A code map of a Java source line.
+ * <p></p>
+ * A code map represents each character of a source code 
+ * by the type of it; 
+ * a type of character is one of the following types:<br>
+ * (1) a character in a code, 
+ * (2) a white space, 
+ * (3a) a character within a line comment ("//" style),
+ * (3b) a character within a traditional comment (C style comment),
+ * (3c) a character within a javadoc,
+ * (4a) a character within a commented code (line comment), and
+ * (4b) a character within a commented code (C style comment).
+ * 
+ * @author Hirohisa AMAN &lt;aman@computer.org&gt;
+ */
+public class CodeLineMapForJava 
+extends CodeLineMap
+{
+
+    /** code map code for a line comment (// style) */
+    public static final String EOL_COMMENT = "2";
+    
+    /** code map code for a commented code in a line comment */
+    public static final String EOL_COMMENT_OUT = "5";
+    
+    /** code map code for a javadoc */
+    public static final String JAVADOC_COMMENT = "4";
+
+    /** code map code for a traditional (C style) comment */
+    public static final String TRADITIONAL_COMMENT = "3";
+    
+    /** code map code for a commented code in a traditiona (C style) comment */
+    public static final String TRADITIONAL_COMMENT_OUT = "6";
+    
+    /**
+     * Constructs a code map of a Java source line.
+     * 
+     * @param aMap a code map of a Java source line
+     */
+    public CodeLineMapForJava(final String aMap)
+    {
+        super(aMap);
+        
+        for (int i = 0; i < aMap.length(); i++ ){
+            String ithChar = Character.toString(aMap.charAt(i));
+            if ( EOL_COMMENT.equals(ithChar) ){
+                eolCommentCount++;
+            }
+            else if ( TRADITIONAL_COMMENT.equals(ithChar) ){
+                traditionalCommentCount++;
+            }
+            else if ( JAVADOC_COMMENT.equals(ithChar) ){
+                javadocCommentCount++;
+            }
+            else if ( EOL_COMMENT_OUT.equals(ithChar) ){
+                eolCommentOutCount++;
+            }
+            else if ( TRADITIONAL_COMMENT_OUT.equals(ithChar) ){
+                traditionalCommentOutCount++;
+            }
+        }
+    }
+
+    /**
+     * Retrieves the number of characters belong to EOL_COMMENT type.
+     * 
+     * @return the number of characters belong to EOL_COMMENT type
+     */
+    public int getEolCommentCount()
+    {
+        return eolCommentCount;
+    }
+    
+    /**
+     * Retrieves the number of characters belong to EOL_COMMENT_OUT type.
+     * 
+     * @return the number of characters belong to EOL_COMMENT_OUT type
+     */
+    public int getEolCommentOutCount()
+    {
+        return eolCommentOutCount;
+    }
+
+    /**
+     * Retrieves the number of characters belong to JAVADOC_COMMENT type.
+     * 
+     * @return the number of characters belong to JAVADOC_COMMENT type
+     */
+    public int getJavadocCommentCount()
+    {
+        return javadocCommentCount;
+    }
+
+    /**
+     * Retrieves the number of characters belong to TRADITIONAL_COMMENT type.
+     * 
+     * @return the number of characters belong to TRADITIONAL_COMMENT type
+     */
+    public int getTraditionalCommentCount()
+    {
+        return traditionalCommentCount;
+    }
+
+    /**
+     * Retrieves the number of characters belong to TRADITIONAL_COMMENT_OUT type.
+     * 
+     * @return the number of characters belong to TRADITIONAL_COMMENT_OUT type
+     */
+    public int getTraditionalCommentOutCount()
+    {
+        return traditionalCommentOutCount;
+    }
+
+    /**
+     * Returns the string representation of this CodeLineMapForJava in the following format:
+     * <br>
+     *  (b?,c?,#?:t?,e?,j?,ec?,tc?) ....... 
+     * <br>
+     * The first three ? parts are the character counts of blanks, code and comments, respectively.
+     * The remaining ? parts are the details of the comment counts:
+     * the character counts of traditional comments, eol comments, javadocs, eol-style commented code, 
+     * traditional-style commented code.
+     * <br>
+     * The ...... part corresponds to the code map.
+     * 
+     * @return the string representation of this CodeLineMapForJava
+     */
+    public String toString()
+    {
+        StringBuffer buf = new StringBuffer();        
+        buf.append("(b" + getBlankCount() + ",c" + getCodeCount() + ",#" + getCommentCount() + ":");
+        buf.append("t" + getTraditionalCommentCount() + ",");
+        buf.append("e" + getEolCommentCount() + ",");
+        buf.append("j" + getJavadocCommentCount() + ",");
+        buf.append("ec" + getEolCommentOutCount() + ",");
+        buf.append("tc" + getTraditionalCommentOutCount() + ")");
+        buf.append(" ");
+        buf.append(getMap());
+        
+        return new String(buf);
+    }
+
+    /** the number of characters corresponding to EOL_COMMENT */
+    private int eolCommentCount;
+    
+    /** the number of characters corresponding to EOL_COMMENT_OUT */
+    private int eolCommentOutCount;
+
+    /** the number of characters corresponding to JAVADOC_COMMENT */
+    private int javadocCommentCount;
+
+    /** the number of characters corresponding to TRADITIONAL_COMMENT */
+    private int traditionalCommentCount;
+
+    /** the number of characters corresponding to TRADITIONAL_COMMENT_OUT */
+    private int traditionalCommentOutCount;
+}
---
+++ src/org/computer/aman/metrics/util/java/CodeMapForJava.java
@@ -0,0 +1,309 @@
+package org.computer.aman.metrics.util.java;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import org.computer.aman.io.sourcecode.SourceFile;
+import org.computer.aman.metrics.util.CodeLineMap;
+import org.computer.aman.metrics.util.CodeMap;
+
+/**
+ * A code map representing the content of a Java source code.
+ * <p></p>
+ * A code map is a character-level map representing the content of a Java source file or a code fragment,
+ * in which each of the elements is an integer meaning one of the followings: 
+ * <ul>
+ *  <li> 0: a white space </li>
+ *  <li> 1: a source code (not blank, not comment)</li>
+ *  <li> 2: a EOL comment (// ...) </li>
+ *  <li> 3: a traditional comment ( /* .. ) </li>
+ *  <li> 4: a Javadoc comment</li>
+ * </ul>
+ * <p></p>
+ * An instance of CodeMapJava contains a list of CodeLineMapForJava which is 
+ * a code line map for Java code.
+ * 
+ * @author Hirohisa AMAN &lt;aman@computer.org&gt;
+ */
+public class CodeMapForJava 
+extends CodeMap
+{
+	private CodeMapForJava()
+	{
+		status = CODE;
+		workingMapList = new LinkedList<String>();
+		workingMap = new StringBuilder();
+		commentContents = new StringBuilder();
+	}
+	
+    /**
+     * Creates a CodeMap object for a Java source file.
+     * 
+     * @param aSourceFile the source file 
+     * @throws IOException if the reading of the source file is failed.
+     */
+    public CodeMapForJava(final SourceFile aSourceFile) 
+    throws IOException
+    {
+    	this();
+        
+        LineNumberReader reader = new LineNumberReader(new FileReader(aSourceFile));
+        String line = null;
+        while ( (line = reader.readLine()) != null ){
+        	parseLine(line);
+        }
+
+        for (Iterator<String> iterator = workingMapList.iterator(); iterator.hasNext();) {
+            add(new CodeLineMapForJava(iterator.next()));            
+        }
+
+        reader.close();
+    }    
+
+    /**
+     * Creates a CodeMap object for a Java code fragment (String array).
+     * 
+     * @param sourceLines Array of Java code lines
+     */
+    public CodeMapForJava(final String[] sourceLines)
+    {
+    	this();
+
+    	for ( int i = 0; i < sourceLines.length; i++  ){
+        	parseLine(sourceLines[i]);
+        }
+
+        for (Iterator<String> iterator = workingMapList.iterator(); iterator.hasNext();) {
+            add(new CodeLineMapForJava(iterator.next()));            
+        }
+    }
+
+    /**
+    * Returns true if the given string seems to be commented code (comment-out code).
+    * 
+    * @param aLine string to be checked
+    * @return true if the given string seems to be commented code
+    */
+    public boolean isCommentOut(final String aLine)
+    {
+        // Regard the line to be a commented code if the line ends with "{", "}" or ";",
+    	// where "*/" is omitted.
+    	char tail = ' ';
+    	String str = aLine.trim();
+    	if ( str.endsWith("*/") ){
+    		int idx = str.length()-3;
+    		while ( idx >= 0 ){
+    			char ch = str.charAt(idx);
+    			if ( ch != '*' && !Character.isWhitespace(ch) ){
+    				tail = ch;
+    				break;
+    			}
+    			idx--;
+    		}
+    	}
+        return ( tail == '{' || tail == '}' || tail == ';' );
+    }
+    
+    /**
+     * Append the specified type of element to the working map (workingMap)
+     * "aCount" times.
+     * 
+     * @param aTypeOfElement the type of map element
+     * @param aCount repeat count
+     */
+    private void appendToWorkingMap(final String aTypeOfElement, final int aCount)
+    {
+        for ( int j = 0; j < aCount; j++ ){
+            workingMap.append(aTypeOfElement);
+        }
+    }
+    
+    /**
+     * Parses the specified source line and create the corresponding code map.
+     * Since a statement or a comment block can be across two or more lines,
+     * this method maintains the state of the parsing with using fields. 
+     * 
+     * @param aSourceLine source line to be parsed
+     */
+	private void parseLine( String aSourceLine )
+	{
+        int idx = 0;
+        while ( idx < aSourceLine.length() ){    
+            char ch = aSourceLine.charAt(idx);
+            
+            if ( Character.isWhitespace(ch) ){ 
+                if ( status == TRADITIONAL ){
+                	appendToWorkingMap(CodeLineMapForJava.TRADITIONAL_COMMENT, 1);
+                    commentContents.append(ch);
+                }
+                else if ( status == STRING_LITERAL || status == CHAR_LITERAL ){
+                	appendToWorkingMap(CodeLineMap.CODE, 1);
+                }
+                else if ( status == JAVADOC ){
+                	appendToWorkingMap(CodeLineMapForJava.JAVADOC_COMMENT, 1);
+                }
+                else{                            
+                	appendToWorkingMap(CodeLineMap.BLANK, 1);
+                }
+                idx++;
+                continue;
+            }
+
+            if ( status == CODE ){ 
+                if ( ch == '/' ){ 
+                    String remainedPart = aSourceLine.substring(idx);
+                    if ( remainedPart.startsWith("//") ){
+                        idx += remainedPart.length();
+                        appendToWorkingMap(isCommentOut(trimComment(remainedPart)) ? CodeLineMapForJava.EOL_COMMENT_OUT : CodeLineMapForJava.EOL_COMMENT, remainedPart.length());
+                    }
+                    else if ( remainedPart.startsWith("/**") ){
+                        status = JAVADOC;
+                        idx += "/**".length();
+                        appendToWorkingMap(CodeLineMapForJava.JAVADOC_COMMENT, "/**".length());
+                    }
+                    else if ( remainedPart.startsWith("/*") ){
+                        status = TRADITIONAL;
+                        idx += "/*".length();
+                        appendToWorkingMap(CodeLineMapForJava.TRADITIONAL_COMMENT, "/*".length());
+                        commentContents.delete(0, commentContents.length());
+                        commentContents.append("/*");
+                    }
+                    else{
+                        idx++;
+                        appendToWorkingMap(CodeLineMapForJava.CODE, 1);
+                    }
+                }
+                else{
+                    if ( ch == '\''){
+                        status = CHAR_LITERAL;
+                    }
+                    else if ( ch == '"' ){
+                        status = STRING_LITERAL;
+                    }                        
+                    idx++;
+                    appendToWorkingMap(CodeLineMapForJava.CODE, 1);          
+                }
+                continue;
+            }
+            
+            if ( status == TRADITIONAL ){
+                if ( ch == '*' && idx+1 < aSourceLine.length() && aSourceLine.charAt(idx+1) == '/' ){
+                    idx += "*/".length();
+                    appendToWorkingMap(CodeLineMapForJava.TRADITIONAL_COMMENT, "*/".length());
+                    commentContents.append("*/");
+                    status = CODE;
+                    if ( isCommentOut(trimComment(new String(commentContents))) ){
+                        // replace the consecutive TRADITIONAL_COMMENT elements with TRADITIONAL_COMMENT_OUT elements 
+                    	// in the working map
+                        int length = commentContents.length();
+                        for ( int i = workingMap.length()-1; i >= 0; i-- ){
+                            workingMap.replace(i, i+1, CodeLineMapForJava.TRADITIONAL_COMMENT_OUT);
+                            length--;
+                            if ( length == 0 ){
+                                break;
+                            }
+                        }
+                        LinkedList<String> replacedMapList = new LinkedList<String>();
+                        while ( length > 0 ){
+                            StringBuilder buf = new StringBuilder(workingMapList.removeLast());
+                            for ( int i = buf.length()-1; i >= 0; i-- ){
+                                buf.replace(i, i+1, CodeLineMapForJava.TRADITIONAL_COMMENT_OUT);
+                                length--;
+                                if ( length == 0 ){
+                                    break;
+                                }
+                            }
+                            replacedMapList.addLast(new String(buf));
+                        }
+                        if ( replacedMapList.size() > 0 ){
+                            workingMapList.addAll(replacedMapList);
+                        }
+                    }
+                }
+                else{
+                    idx++;
+                    appendToWorkingMap(CodeLineMapForJava.TRADITIONAL_COMMENT, 1);
+                    commentContents.append(ch);
+                }
+                continue;
+            }
+            
+            if ( status == JAVADOC ){
+                if ( ch == '*' && idx+1 < aSourceLine.length() && aSourceLine.charAt(idx+1) == '/' ){
+                    idx += "*/".length();
+                    appendToWorkingMap(CodeLineMapForJava.JAVADOC_COMMENT, "*/".length());
+                    status = CODE;
+                }
+                else{
+                    idx++;
+                    appendToWorkingMap(CodeLineMapForJava.JAVADOC_COMMENT, 1);
+                }
+                continue;
+            }
+            
+            if ( status == CHAR_LITERAL ){
+                idx++;
+                appendToWorkingMap(CodeLineMapForJava.CODE, 1);
+                if ( ch == '\\' ){ 
+                    idx++;
+                    appendToWorkingMap(CodeLineMapForJava.CODE, 1);
+                }
+                else if ( ch == '\'' ){
+                    status = CODE;
+                }
+                continue;
+            }
+            
+            if ( status == STRING_LITERAL ){
+                idx++;
+                appendToWorkingMap(CodeLineMapForJava.CODE, 1);
+                if ( ch == '\\' ){
+                    idx++;
+                    appendToWorkingMap(CodeLineMapForJava.CODE, 1);
+                }
+                else if ( ch == '"' ){
+                    status = CODE;
+                }
+                continue;
+            }
+        }
+
+        workingMapList.add(new String(workingMap));
+        workingMap.delete(0, workingMap.length());
+	}
+    
+
+    /**
+     * Erases the comment starting symbols and the ending symbols 
+     * from the specified comment string.
+     * 
+     * @param aComment comment string to be processed
+     * @return trimmed comment string
+     */
+    private String trimComment(final String aComment)
+    {
+        if ( aComment.startsWith("//") ){
+            return aComment.substring(2);
+        }
+        if ( aComment.startsWith("/*") ){
+            return aComment.substring(2, aComment.length()-2);
+        }
+        return null;
+    }
+    
+
+    private int status;
+
+    private final int CODE = 1;
+    private final int TRADITIONAL = 12;
+    private final int JAVADOC = 13;
+    private final int STRING_LITERAL = 100;
+    private final int CHAR_LITERAL = 200;
+    
+    private StringBuilder commentContents;
+    private LinkedList<String> workingMapList;
+    private StringBuilder workingMap;
+}
